be4ba98a6614e1f7d6bed63a16e489c8579d0af2
Davide <43414189+Demiurgo443@users.noreply.github.com>
2023-03-29 01:00:51 +0200

    Added setters of node attributes

--- node.py
+++ node.py
@@ -6,3 +6,17 @@ class Node:
         self.parents: list[list[str, int]] | list[str, int] # [[id, not], ...]  not -> 0 = normale - 1 = esegue il not
         self.children = []  # per children si intende nodi "superiori" al nodo corrente, quindi più vicini alla radice
         self.leaves = set()
+
+    def add_child(self, child: str):
+        self.children.append(child)
+
+    # specifica tutti i nodi variabile coinvolti direttamente e indirettamente dal nodo
+    def add_leaves(self, leaves: set):
+        self.leaves.update(leaves)
+
+    def set_value(self, value: bool):  # cambio valore per nodi foglia
+        self.value = value
+
+    def set_operation(self, parents, op=None):
+        self.op = op
+        self.parents = parents
