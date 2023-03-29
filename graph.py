f5e764fe64f5ba91cad384407ca00a8a05404bd7
Davide <43414189+Demiurgo443@users.noreply.github.com>
2023-03-29 15:47:03 +0200

    Added method to run graph and its config

--- graph.py
+++ graph.py
@@ -22,3 +22,28 @@ class Graph:
             for parent in self.nodes[child].parents:
                 if parent[0] == node_id:
                     parent[1] = not parent[1]
+
+    def run_input(self, val: int | float, truth_table: dict[str, dict[str, bool]]) -> dict[str, dict[str, bool]]:
+        input_str = format(val, 'b')  # Binary format. Outputs the number in base 2
+        while len(input_str) < self.input_num:
+            input_str = "0" + input_str
+
+        # se nella truth table non c'è una voce corrispondente (riga specifica della tabella di verità), la crea
+        # es.: ttable = {}. Non c'è 0000. Allora faccio inserimento e ttable diventa {'0000': {}}
+        if not hasattr(truth_table, input_str):
+            truth_table[input_str] = {}
+
+        # assegna i valori alle foglie (variabili input),
+        # in base al loro varole nella stringa che identifica la riga della truth table
+        # - input_str[idx]: valore attuale variabile (0 o 1)
+        # - node_id: identificativo variabile (es.: x0)
+        for idx, node_id in enumerate(self.leaves):
+            self.nodes[node_id].set_value(bool(int(input_str[idx])))
+
+        # calcola i valori finali di output per tutte le variabili in output
+        # truth_table[input_str][out]: risultato (True o False)
+        for out in self.roots:
+            # in caso di singolo output nodes[out] è il nodo y0
+            truth_table[input_str][out] = self.nodes[out].run(self.nodes)
+
+        return truth_table
--- main.py
+++ main.py
@@ -19,3 +19,8 @@ if __name__ == '__main__':
     # xag2, nodes_of_interest2 = parser("verilogs/e_pla_4i_5_xor.v_opt.v")
     xag, nodes_of_interest = parser("verilogs/e_pla_6i_1.v_opt.v")
     xag2, nodes_of_interest2 = parser("verilogs/e_pla_6i_1.v_opt.v")
+
+    max_num = 2 ** xag.input_num
+    for val in range(max_num):
+        truth_table = xag.run_input(val, truth_table)
+    print(truth_table)
