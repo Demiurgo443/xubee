499cb38c6701d2fa7d8fc739776975227ffca8a3
Davide <43414189+Demiurgo443@users.noreply.github.com>
2023-03-29 15:20:12 +0200

    Added method to modify node operations in graphs
    - changing AND in XNOR

--- graph.py
+++ graph.py
@@ -11,3 +11,14 @@ class Graph:
     @property
     def input_num(self):
         return len(self.leaves)
+
+    # L'operazione di cambio consiste nella trasformazione di AND in XNOR.
+    def set_change(self, node_id: str):
+        # innanzitutto cambio op (ha solo val 0 e 1 - xor e and)
+        self.nodes[node_id].op = not self.nodes[node_id].op
+        # per rendere effettivo lo XNOR, vado nei nodi figli (superiori) e faccio sì che
+        # il nodo modificato entri loro negato
+        for child in self.nodes[node_id].children:
+            for parent in self.nodes[child].parents:
+                if parent[0] == node_id:
+                    parent[1] = not parent[1]
