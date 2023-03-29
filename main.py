c5e76c955c661284c59fdf5387ffbfe11e416956
Davide <43414189+Demiurgo443@users.noreply.github.com>
2023-03-29 14:34:52 +0200

    Created utility, main and their basic structure
    - utility: has document parser, which will return Graph object
    - main: will take parser output and will perform error estimation and experimental comparisons between truth tables

---
+++ main.py
@@ -0,0 +1,9 @@
+# Xag Upper Bound Error Evaluation (valuta se inserire uno fra i seguenti termini: Experimental, Estimation, Framework)
+
+# import sys
+from utility import parser
+
+if __name__ == '__main__':
+    truth_table = {}
+    truth_table_2 = {}
+    error_list = {}
---
+++ utility.py
@@ -0,0 +1,15 @@
+from node import Node
+from graph import Graph
+
+
+def parser(file_name: str) -> tuple[Graph, list[str]]:
+
+    input_list = []
+    internal_list = []
+    output_list = []
+    node_list = {}
+    interest_list = []
+
+
+
+    return Graph(input_list, internal_list, output_list, node_list), interest_list
