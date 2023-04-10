d8f4c96e0f350c68eece3d333ba8bdb99435758c
Davide <43414189+Demiurgo443@users.noreply.github.com>
2023-04-10 12:24:15 +0200

    Optimized main
    - Removed second call to parser, implemented deepcopy() instead.

--- main.py
+++ main.py
@@ -3,18 +3,17 @@
 import sys
 from utility import parser, to_file
 from core import error_estimation_4i, error_estimation
+from copy import deepcopy
 
 if __name__ == '__main__':
 
     # makes this program launchable with bash scripts
     filename = sys.argv[1]
 
-    # xag, nodes_of_interest = parser("verilogs10i/e_pla_10i_16.v_opt.v")
     # xag2, nodes_of_interest2 = parser("verilogs10i/e_pla_10i_16.v_opt.v")
-    # xag, nodes_of_interest = parser("miei_verilogs/e_pla_6i_001.v_opt.v")
     # xag2, nodes_of_interest2 = parser("miei_verilogs/e_pla_6i_001.v_opt.v")
-    xag, nodes_of_interest = parser(f"{filename}")
-    xag2, nodes_of_interest2 = parser(f"{filename}")
+    xag2, nodes_of_interest = parser(f"{filename}")
+    xag = deepcopy(xag2)
 
     filename = filename.removeprefix("/home/davide/PycharmProjects/xubee/verilogs/")
 
@@ -27,7 +26,7 @@ if __name__ == '__main__':
     else:
         if xag2.input_num == 4:
             estimated_e_dict, exact_e_dict, f_elapsed_time, tt_elapsed_time = error_estimation_4i(xag2, xag,
-                                                                                                  nodes_of_interest2)
+                                                                                                  nodes_of_interest)
             if len(estimated_e_dict) == 0 and len(exact_e_dict) == 0:
                 print("Nessun nodo adatto trovato in fase di stima dell'errore")
             else:
@@ -36,7 +35,7 @@ if __name__ == '__main__':
                 to_file(filename, xag, estimated_e_dict, exact_e_dict, f_elapsed_time, tt_elapsed_time)
         else:
             estimated_e_dict, exact_e_dict, f_elapsed_time, tt_elapsed_time = error_estimation(xag2, xag,
-                                                                                               nodes_of_interest2)
+                                                                                               nodes_of_interest)
             if len(estimated_e_dict) == 0 and len(exact_e_dict) == 0:
                 print("Nessun nodo adatto trovato in fase di stima dell'errore")
             else:
